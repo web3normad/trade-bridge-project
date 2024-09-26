a3f3575097ffa211aa2dac9eba3a33f7de9c3dc5
morelucks <luckykamshak@gmail.com>
2024-09-26 22:22:22 +0100

    trade-bridge token

---
+++ contract/contracts/Token.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract TradeBidge is ERC20, Ownable {
+    constructor(
+        address initialOwner
+    ) ERC20("TradeBidge", "TBTK") Ownable(initialOwner) {}
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
