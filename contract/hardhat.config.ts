25c26fa1d309233bfcf57534aa0ec30be968ae65
Kefas Kingsley <kefasiceking@gmail.com>
2024-09-27 12:31:45 +0100

    sync: Pushed all contracts

--- contract/hardhat.config.ts
+++ contract/hardhat.config.ts
@@ -13,28 +13,6 @@ const config: HardhatUserConfig = {
       viaIR: true
     }
   },
-  networks: {
-    'lisk-sepolia': {
-      url: 'https://rpc.sepolia-api.lisk.com',
-      accounts: [process.env.PRIVATE_KEY as string],
-      gasPrice: 1000000000,
-    },
-  },
-  etherscan: {
-    apiKey: {
-      "lisk-sepolia": "123"
-    },
-    customChains: [
-      {
-          network: "lisk-sepolia",
-          chainId: 4202,
-          urls: {
-              apiURL: "https://sepolia-blockscout.lisk.com/api",
-              browserURL: "https://sepolia-blockscout.lisk.com"
-          }
-      }
-    ]
-  },
   sourcify: {
     enabled: false
   },
